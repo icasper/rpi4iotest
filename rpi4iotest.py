9cc500634b2345dc350c06aefb842e7b0829d1c6
icasper <ian.casper.za@gmail.com>
2021-03-09 08:04:02 +0200

    Added exit button to window

--- rpi4iotest.py
+++ rpi4iotest.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+def exitApp():
+    import sys
+    sys.exit()
+
+root = tk.Tk()
 root.title('Raspberry pi IO Test v0.1a')
 root.geometry('400x200+700+400')
+button1 = tk.Button(root, text = 'Exit', command = exitApp)
+button1.pack()
 
 # main loop
 
