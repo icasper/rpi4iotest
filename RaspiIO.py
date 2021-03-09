a7025043fe0fca212aebc58c9f9dfa286e78703d
icasper <ian.casper.za@gmail.com>
2021-03-09 12:58:26 +0200

    Updated Class

--- RaspiIO.py
+++ RaspiIO.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
-class RaspiIO(self, master):
-    self.master = master
-    
+class Raspiio(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.geometry('500x500+700+400')
+        self.title('Raspberry Pi I/O v0.1a')
+        
+raspiio = Raspiio()
+raspiio.mainloop()
