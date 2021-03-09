7ab901f397888dc5352becb26808425ec988fea7
icasper <ian.casper.za@gmail.com>
2021-03-09 15:28:45 +0200

    Updated Raspiio class

--- RaspiIO.py
+++ RaspiIO.py
@@ -3,8 +3,22 @@ from tkinter import *
 
 class Raspiio:
     def __init__(self, win):
+        self.var = IntVar()
         self.button1 = Button(win,text='Exit',command=self.exitNow)
         self.button1.place(x = 165, y = 250)
+        self.r1 = Radiobutton(win,text='Red LED', variable=self.var, value=1, command=self.rl)
+        self.r2 = Radiobutton(win,text='Green LED', variable=self.var, value=2, command=self.rl)
+        self.r3 = Radiobutton(win,text='Blue LED',variable=self.var, value=3, command=self.rl)
+        self.r1.place(x=10,y=10)
+        self.r2.place(x=10,y=30)
+        self.r3.place(x=10,y=50)
+        self.label = Label(win)
+        self.label.pack(side='bottom')
+        
+    def rl(self):
+       selection = "You selected the option " + str(self.var.get())
+       self.label.config(text = selection)
+        
         
     def exitNow(self):
         import sys
