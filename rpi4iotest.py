bbf641d8db95554f66687a2bdab7392274f029a7
icasper <ian.casper.za@gmail.com>
2021-03-09 08:43:18 +0200

    Added buttons with grid geometry manager

--- rpi4iotest.py
+++ rpi4iotest.py
@@ -5,12 +5,17 @@ import tkinter as tk
 def exitApp():
     import sys
     sys.exit()
+    
+def executeApp():
+    pass
 
 root = tk.Tk()
 root.title('Raspberry pi IO Test v0.1a')
 root.geometry('400x200+700+400')
 button1 = tk.Button(root, text = 'Exit', command = exitApp)
-button1.pack()
+button1.grid(row = 3, column = 1, sticky = tk.W)
+button2 = tk.Button(root, text = 'Execute', command = executeApp)
+button2.grid(row = 2, column = 1, sticky = tk.W)
 
 # main loop
 
